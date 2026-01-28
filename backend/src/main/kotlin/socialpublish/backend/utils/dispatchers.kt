356e418a99ada4c54e8e5f5c5f2007a5110eeb31
Alexandru Nedelcu <noreply@alexn.org>
2026-01-28 17:38:59 +0200

    Small fix for Dispatchers.LoomIO

--- backend/src/main/kotlin/socialpublish/backend/utils/dispatchers.kt
+++ backend/src/main/kotlin/socialpublish/backend/utils/dispatchers.kt
@@ -14,26 +14,26 @@ import kotlinx.coroutines.ExecutorCoroutineDispatcher
  *
  * Requires Java 21 or later.
  */
+@Suppress("UnusedReceiverParameter")
 val Dispatchers.LoomIO: CoroutineDispatcher
     get() = LOOM_DISPATCHER
 
-private val LOOM_DISPATCHER: CoroutineDispatcher
-    get() =
-        object : ExecutorCoroutineDispatcher(), Executor {
-            override val executor: Executor
-                get() = this
+private val LOOM_DISPATCHER: CoroutineDispatcher =
+    object : ExecutorCoroutineDispatcher(), Executor {
+        override val executor: Executor
+            get() = this
 
-            override fun close() {
-                error("Cannot be invoked on Dispatchers.LOOM")
-            }
-
-            override fun dispatch(context: CoroutineContext, block: Runnable) {
-                Thread.startVirtualThread(block)
-            }
-
-            override fun execute(command: Runnable) {
-                Thread.startVirtualThread(command)
-            }
-
-            override fun toString() = "Dispatchers.LOOM"
+        override fun close() {
+            error("Cannot be invoked on Dispatchers.LOOM")
         }
+
+        override fun dispatch(context: CoroutineContext, block: Runnable) {
+            Thread.startVirtualThread(block)
+        }
+
+        override fun execute(command: Runnable) {
+            Thread.startVirtualThread(command)
+        }
+
+        override fun toString() = "Dispatchers.LOOM"
+    }
